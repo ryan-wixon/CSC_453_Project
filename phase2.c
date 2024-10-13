1e0f17e3f9e6ecd68de08f65b9dff51675b298c4
Adrianna Koppes <ask033240@gmail.com>
2024-10-13 11:56:48 -0700

    added some disabling/restoring interrupt stuff to the functions that have been started. also created a shadow process structure and finished adding the required data structures, and possibly finished a syscall handler

--- phase2.c
+++ phase2.c
@@ -28,15 +28,33 @@ typedef struct Mailbox {
 	int filledSlots;	// number of slots that are occupied
 
 	//TODO not sure if this should be void*, these should be linked lists of processes
-	void* producers;	// processes that want to write to this mailbox line up here
-	void* consumers;	// processes that want to read from this mailbox line up here
+	Process2* producers;	// processes that want to write to this mailbox line up here
+	Process2* consumers;	// processes that want to read from this mailbox line up here
 	
 } Mailbox;
+
+/* Information for processes in Phase 2 is included here, since we can't
+   add to the Phase 1 process table. */
+typedef struct Process2 {
+	int pid;  // process ID, to match to ensure if a process dies, we can find out
+
+	Process2* nextProducer;	// next process in a send queue for mailbox
+	Process2* nextConsumer; // next process in a receive queue for mailbox
+} Process2;
 
 // global array of mailboxes; a mailbox's ID corresponds to it's index in this array
 Mailbox mailboxes[MAXMBOX];
 int mailboxOccupancies[MAXMBOX];
 int numMailboxes = 0;
+
+// global array of message slots as described in Phase 2 spec
+Message messageSlots[MAXSLOTS];
+
+// shadow process table for Phase 2
+Process2 procTable2[MAXPROC];
+
+// vector of system calls - set every single element to nullsys (see below)
+void (*systemCallVec[MAXSYSCALLS])(USLOSS_Sysargs *args);
 
 int send(int mboxID, void *message, int msgSize, int doesBlock);
 int receive(int mboxID, void *message, int maxMsgSize, int doesBlock);
@@ -45,11 +63,8 @@ void nullsys(void);
 void clockHandler(int type, void *arg);
 void diskHandler(int type, void *arg);
 void terminalHandler(int type, void *arg);
-// TODO IMPLEMENT INTERRUPT HANDLERS (see below)
-
-// TODO ADD THE OTHER REQUIRED DATA STRUCTURES
-// vector of system calls - set every single element to nullsys (see below)
-void (*systemCallVec[MAXSYSCALLS])(USLOSS_Sysargs *args);
+void syscallHandler(int type, void *arg);
+// TODO IMPLEMENT INTERRUPT HANDLERS (see below) -- need to wait for mailbox implementation
 
 void phase2_init(void) {
     unsigned int oldPSR = USLOSS_PsrGet();
@@ -72,6 +87,7 @@ void phase2_init(void) {
     USLOSS_IntVec[USLOSS_CLOCK_INT] = clockHandler;
     USLOSS_IntVec[USLOSS_TERM_INT] = terminalHandler;
     USLOSS_IntVec[USLOSS_DISK_INT] = diskHandler;
+	USLOSS_IntVec[USLOSS_SYSCALL_INT] = syscallHandler;
 
     // TODO - implement the rest of phase2_init
 
@@ -109,6 +125,14 @@ void terminalHandler(int type, void *arg) {
     // TODO
 }
 
+void syscallHandler(int type, void *arg) {
+	// may want to disable interrupts??
+	USLOSS_Sysargs callArgs = (USLOSS_Sysargs*)arg;	// may need to fix syntax here
+	int index = callArgs->number;
+	systemCallVec[index](void);
+	// if need to disable interrupts in this function, restore interrupts here
+}
+
 /*
  * Handler for system calls. We are not working with syscalls yet
  * (wait until Phase 3), so this "handler" simply prints an error
@@ -124,6 +148,19 @@ void nullsys(void) {
 }
 
 int MboxCreate(int slots, int slot_size) {
+	// disable/restore interrupts
+	unsigned int oldPSR = USLOSS_PsrGet();
+    if(oldPSR % 2 == 0) {
+        // we cannot call this function in user mode
+        fprintf(stderr, "ERROR: Someone attempted to call MboxCreate while in user mode!\n");
+        USLOSS_Halt(1);
+    }
+    if(oldPSR % 4 == 3) {
+        // interrupts enabled...we need to disable them
+        if (USLOSS_PsrSet(oldPSR - 2) == USLOSS_ERR_INVALID_PSR) {
+            fprintf(stderr, "Bad PSR set in MboxCreate\n");
+        }
+    }
 
 	if (slots < 0 || slot_size < 0 || slots > MAXSLOTS || slot_size > MAX_MESSAGE) {
 		fprintf(stderr, "ERROR: Bad arguments given to MboxCreate.\n");
@@ -143,6 +180,10 @@ int MboxCreate(int slots, int slot_size) {
 	for (int i = 0; i < MAXMBOX; i++) {
 		if (mailboxes[i] != NULL) {
 			mailboxes[i] = newMailbox;
+			// restore old PSR
+    		if (USLOSS_PsrSet(oldPSR) == USLOSS_ERR_INVALID_PSR) {
+        		fprintf(stderr, "Bad PSR restored in MboxCreate\n");
+    		}
 			return i;	
 		}
 	}
@@ -150,24 +191,63 @@ int MboxCreate(int slots, int slot_size) {
 	// don't do anything if there is no space for the mailbox
 	fprintf(stderr, "ERROR: No space to create new mailbox.\n");
 
+	// restore old PSR
+    if (USLOSS_PsrSet(oldPSR) == USLOSS_ERR_INVALID_PSR) {
+        fprintf(stderr, "Bad PSR restored in MboxCreate\n");
+    }
+
 	return -1;
 }
 
 int MboxRelease(int mbox_id) {
+	// disable/restore interrupts
+	unsigned int oldPSR = USLOSS_PsrGet();
+    if(oldPSR % 2 == 0) {
+        // we cannot call this function in user mode
+        fprintf(stderr, "ERROR: Someone attempted to call MboxRelease while in user mode!\n");
+        USLOSS_Halt(1);
+    }
+    if(oldPSR % 4 == 3) {
+        // interrupts enabled...we need to disable them
+        if (USLOSS_PsrSet(oldPSR - 2) == USLOSS_ERR_INVALID_PSR) {
+            fprintf(stderr, "Bad PSR set in MboxRelease\n");
+        }
+    }
 	
 	if (mailboxes[mbox_id] != NULL) {
 		free(mailboxes[mbox_id].slots);
 		mailboxes[mbox_id] = NULL;
 
 		//TODO More stuff probably
+		// restore old PSR
+    	if (USLOSS_PsrSet(oldPSR) == USLOSS_ERR_INVALID_PSR) {
+        	fprintf(stderr, "Bad PSR restored in MboxRelease\n");
+    	}
 		return 0;
 	}
 	
 	fprintf(stderr, "ERROR: Attempting to release a mailbox which does not exist.\n")
+	// restore old PSR
+    if (USLOSS_PsrSet(oldPSR) == USLOSS_ERR_INVALID_PSR) {
+        fprintf(stderr, "Bad PSR restored in MboxRelease\n");
+    }
 	return -1;
 }
 
 int MboxSend(int mbox_id, void *msg_ptr, int msg_size) {
+	// disable/restore interrupts
+	unsigned int oldPSR = USLOSS_PsrGet();
+    if(oldPSR % 2 == 0) {
+        // we cannot call this function in user mode
+        fprintf(stderr, "ERROR: Someone attempted to call MboxSend while in user mode!\n");
+        USLOSS_Halt(1);
+    }
+    if(oldPSR % 4 == 3) {
+        // interrupts enabled...we need to disable them
+        if (USLOSS_PsrSet(oldPSR - 2) == USLOSS_ERR_INVALID_PSR) {
+            fprintf(stderr, "Bad PSR set in MboxSend\n");
+        }
+    }
 
 	// package the bytes of the message into a new message
 	Message newMessage;
@@ -183,6 +263,7 @@ int MboxSend(int mbox_id, void *msg_ptr, int msg_size) {
 		// there is no slot for the message, the sender needs to join the producer queue and block
 		// TODO actually do that
 	}
+	// TODO RESTORE INTERRUPTS
 }
 
 int MboxRecv(int mbox_id, void *msg_ptr, int msg_max_size) {
