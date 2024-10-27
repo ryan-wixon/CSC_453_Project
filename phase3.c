b28e3e11af1bd6f0f68a6b7fe3fe0883a3ad0053
Adrianna Koppes <ask033240@gmail.com>
2024-10-27 12:11:57 -0700

    hopefully finished implementation of getTime, getPID, and semaphore related syscall functions, but have not tested yet

--- phase3.c
+++ phase3.c
@@ -14,6 +14,12 @@
 #include <phase1.h>  /* to access phase 1 functions, if necessary */
 #include <phase2.h>  /* to access phase2 functions, for mailboxes */
 
+/* semaphore struct -- can change later if necessary */
+typedef struct Semaphore {
+    int count;  /* value of semaphore */
+    int blockOn;    /* ID of the mailbox to block on if count = 0 */
+} Semaphore;
+
 /* function declarations */
 void phase3_start_service_processes();
 void spawn(USLOSS_Sysargs *args);
@@ -25,8 +31,26 @@ void v(USLOSS_Sysargs *args);
 void getTime(USLOSS_Sysargs *args);
 void getPID(USLOSS_Sysargs *args);
 
+/* mailbox lock functions */
+void getLock();
+void releaseLock();
+
+int lock = -1;  /* the ID of the mailbox used as a lock */
+
+#define EMPTY         0
+#define FULL         1
+
+Semaphore semaphores[MAXSEMS];  /* array of available semaphores */
+int filledSems[MAXSEMS];
+int allFull = EMPTY;
+
 void phase3_init() {
     // TODO
+    // create lock
+    lock = MboxCreate(1, 0);
+    // initialize filledSems and semaphores to 0
+    memset(semaphores, 0, sizeof(semaphores));
+    memset(filledSems, 0, sizeof(filledSems));
     // install the proper functions in the syscallVec (from phase 2)
     systemCallVec[SYS_SPAWN] = spawn;
     systemCallVec[SYS_WAIT] = wait;
@@ -41,39 +65,133 @@ void phase3_init() {
 }
 
 void phase3_start_service_processes() {
-    // TODO
+    // TODO - this will be do nothing if we don't have service processes
 }
 
 /* Below: All functions called by the syscall handler. */
 
 void spawn(USLOSS_Sysargs *args) {
+    getLock();
     // TODO
+    // note: the only time you can modify PSR is in the trampoline function
+    // for the user-main function, which is not yet implemented.
+    // TODO -- create and implement user-mode trampoline main function!
+    releaseLock();
 }
 
 void wait(USLOSS_Sysargs *args) {
+    getLock();
     // TODO
+    releaseLock();
 }
 
 void terminate(USLOSS_Sysargs *args) {
+    getLock();
     // TODO
+    releaseLock();
 }
 
 void semCreate(USLOSS_Sysargs *args) {
-    // TODO
+    getLock();
+    int start = (int)(long)(args->arg1);
+    if(start < 0) {
+        // bad arguments, return error code
+        args->arg4 = (void*)(long)(-1);
+        releaseLock();
+        return;
+    }
+    else if(allFull == FULL) {
+        // no space remaining
+        args->arg4 = (void*)(long)(-1);
+        releaseLock();
+        return;
+    }
+    int slot = nextAvailableSem();
+    if(slot == -1) {
+        // found out that no space remaining
+        args->arg4 = (void*)(long)(-1);
+        releaseLock();
+        return;
+    }
+    filledSems[slot] = FULL;
+    // keep track of the semaphore's value here
+    semaphores[slot].count = (int)(long)(args->arg1);
+    // mailbox is only used for blocking, not used to store values
+    // this is to save space and only allocate resources we need
+    semaphores[slot].blockOn = MboxCreate(1, 0);
+    args->arg1 = (void*)(long)slot;
+    args->arg4 = (void*)(long)(0);
+    releaseLock();
 }
 
 void p(USLOSS_Sysargs *args) {
-    // TODO
+    getLock();
+    int slot = (int)(long)(args->arg1);
+    if(slot < 0 || filledSems[slot] == EMPTY) {
+        // invalid arguments!
+        args->arg4 = (void*)(long)(-1);
+        releaseLock();
+        return;
+    }
+    // arguments passed validity check, now attempt to decrement
+    if(semaphores[slot].count == 0) {
+        // block!
+        int mailbox = semaphores[slot].blockOn;
+        releaseLock();
+        MboxRecv(mailbox, NULL, 0);
+        // after here, we are awake again.
+        getLock();
+    }
+    semaphores[slot].count--;
+    args->arg4 = (void*)(long)(0);
+    releaseLock();
 }
 
 void v(USLOSS_Sysargs *args) {
+    getLock();
+    int slot = (int)(long)(args->arg1);
+    semaphores[slot].count++;
+    // conditionally send message to unblock potentially blocked
+    // processes (conditional since we have no way to check if
+    // any processes currently blocked)
+    MboxCondSend(slot, NULL, 0);
     // TODO
+    releaseLock();
 }
 
 void getTime(USLOSS_Sysargs *args) {
-    // TODO
+    getLock();
+    int time = currentTime();
+    args->arg1 = (void*)(long)time;
+    releaseLock();
 }
 
 void getPID(USLOSS_Sysargs *args) {
-    // TODO
+    getLock();
+    int procPID = getpid();
+    args->arg1 = (void*)(long)procPID;
+    releaseLock();
+}
+
+void getLock() {
+    // will block if mailbox is full - prevent concurrency
+    MboxSend(lock, NULL, 0);
+}
+
+void releaseLock() {
+    // will block if mailbox is empty, so be careful about when you call this!
+    MboxRecv(lock, NULL, 0);
+}
+
+int nextAvailableSem() {
+    // don't need to lock; we already have the lock
+    for(int i = 0; i < MAXSEMS; i++) {
+        if(filledSems[i] == EMPTY) {
+            // found an unallocated semaphore
+            return i;
+        }
+    }
+    // no available semaphore spaces
+    allFull = FULL;
+    return -1;
 }
